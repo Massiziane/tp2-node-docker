8cc137c29774c4345483039b8f7a0385d8167aab
Mohamed Massinissa, Ziane <e2185817@cmaisonneuve.qc.ca>
2026-04-26 13:19:32 -0400

    Remove node_modules from tracking

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+let counter = 0;
+
+// Home page (counter)
+app.get("/", (req, res) => {
+    res.send(`
+        <h1>🔥 Click Counter</h1>
+        <p>Count: ${counter}</p>
+
+        <button onclick="fetch('/click').then(() => location.reload())">
+            Click me
+        </button>
+
+        <br><br>
+        <a href="/about">About</a>
+    `);
+});
+
+// increment counter
+app.get("/click", (req, res) => {
+    counter++;
+    res.json({ counter });
+});
+
+//  About page
+app.get("/about", (req, res) => {
+    res.send(`
+        <h1>ℹ️ About</h1>
+        <p>This is a simple Node.js + Express application created for a Docker lab assignment.</p>
+        <a href="/">Back home</a>
+    `);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
